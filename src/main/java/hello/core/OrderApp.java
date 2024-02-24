a8da1fead55525e89e9b5253082ea8db374d4f2d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-24 13:08:00 +0900

    feat: Implement to find Spring beans using the Spring container

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,13 +5,15 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext appContext = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+        MemberService memberService = appContext.getBean("memberService", MemberService.class);
+        OrderService orderService = appContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
