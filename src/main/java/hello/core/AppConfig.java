9ec7772c8b2673440c152cd15c05c6e0d25abf7b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-24 13:07:38 +0900

    feat: Implement setting up AppConfig with @Configuration and @Bean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,26 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     /**
      * AppConfig는 애플리케이션의 실제 동작에 필요한 '구현 객체를 생성' 한다.
      * AppConfig는 생성한 객체 인스턴스의 참조를 '생성자를 통해 주입' 해준다.
      */
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
