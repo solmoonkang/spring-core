abec867cc61913258a2f5edf0543e0cc53174a2f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-26 16:41:44 +0900

    chore: Content modification

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,8 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -47,7 +47,7 @@ public class SingletonTest {
     @DisplayName("스프링 컨테이너와 싱글톤")
     void springContainer() {
         ApplicationContext applicationContext =
-                new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
         MemberService memberService1 = applicationContext.getBean("memberService", MemberService.class);
         MemberService memberService2 = applicationContext.getBean("memberService", MemberService.class);
