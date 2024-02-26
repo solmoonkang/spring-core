7259e6dc3d59cfe2ae89db96b8343bd0aeb49ab4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-26 16:41:46 +0900

    chore: Content modification

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -1,18 +1,16 @@
 package hello.core.singleton;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class StatefulServiceTest {
 
     @Test
     void statefulServiceSingleton() {
         ApplicationContext applicationContext =
-                new AnnotationConfigReactiveWebApplicationContext(TestConfig.class);
+                new AnnotationConfigApplicationContext(TestConfig.class);
 
         StatefulService statefulService1 = applicationContext.getBean(StatefulService.class);
         StatefulService statefulService2 = applicationContext.getBean(StatefulService.class);
