30212250165b53bf5463d8a1e418d94e2dc5f678
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-26 16:40:28 +0900

    feat: Implement of singleton in Spring container

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,44 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = applicationContext.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = applicationContext.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = applicationContext.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = applicationContext.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        // 출력값: bean = class hello.core.AppConfig$$SpringCGLIB$$0
+    }
+}
