707f5cdbdc7880395dbf582b319749282f1db00b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-26 17:22:57 +0900

    chore: Register @Component annotation on the implementation

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 이슈로 인해 ConcurrentHashMap을 사용했다.
