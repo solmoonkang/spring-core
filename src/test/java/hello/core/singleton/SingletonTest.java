e27503c09bae02ef0cc824747668f23bbd0a5baf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-26 15:40:32 +0900

    feat: Implement cautions when using the singleton pattern

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,5 +41,22 @@ public class SingletonTest {
         System.out.println("SingletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext applicationContext =
+                new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = applicationContext.getBean("memberService", MemberService.class);
+        MemberService memberService2 = applicationContext.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른 것을 확인한다.
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,38 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext applicationContext =
+                new AnnotationConfigReactiveWebApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = applicationContext.getBean(StatefulService.class);
+        StatefulService statefulService2 = applicationContext.getBean(StatefulService.class);
+
+        // statefulService1.order("userA", 10000);     // ⚠️ ThreadA: 사용자 A가 10,000원을 주문
+        // statefulService2.order("userB", 20000);     // ⚠️ ThreadB: 사용자 B가 10,000원을 주문
+
+        int userAPrice = statefulService1.order("userA", 10000);
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        // ThreadA: 사용자 A가 주문 금액을 조회한다. -> price의 기대값은 10,000원이 나오는 것이지만, 20,000원이 나온다.
+        // int price = statefulService1.getPrice();
+        System.out.println("userA price = " + userAPrice);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
