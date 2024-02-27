e49cc4aaa49b0784d960eaa3be8bba8d2e8719b8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-27 23:07:14 +0900

    chore: Add the final keyword to the field in MemoryMemberRepository

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 이슈로 인해 ConcurrentHashMap을 사용했다.
-    private static Map<Long, Member> store = new ConcurrentHashMap<>();
+    private static final Map<Long, Member> store = new ConcurrentHashMap<>();
 
     @Override
     public void save(Member member) {
